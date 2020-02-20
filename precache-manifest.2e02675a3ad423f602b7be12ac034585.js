2d0bab9d5514c1dca1446d3d31c632548b11b8c3
AlekseiKorolev <42728999+AlekseiKorolev@users.noreply.github.com>
2020-02-20 01:24:35 +0100

    Updates

---
+++ precache-manifest.2e02675a3ad423f602b7be12ac034585.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef5d9a0591dbbc6f0dc9bf1b4adfdcba",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "2eae8b73e2db3cdfe73b",
+    "url": "/test/static/css/main.6c153c3e.chunk.css"
+  },
+  {
+    "revision": "aec3f3118a4493186e5b",
+    "url": "/test/static/js/2.6a4c927c.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/test/static/js/2.6a4c927c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2eae8b73e2db3cdfe73b",
+    "url": "/test/static/js/main.0863c22c.chunk.js"
+  },
+  {
+    "revision": "720b3f3b52cd63e7e740",
+    "url": "/test/static/js/runtime-main.a9446ed3.js"
+  },
+  {
+    "revision": "a6b9124c598ff3253a374ba6b34af3bb",
+    "url": "/test/static/media/city.a6b9124c.jpg"
+  }
+]);
