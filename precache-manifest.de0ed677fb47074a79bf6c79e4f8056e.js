de95e5927c0b091d02f21933dbe21a7d4869ec74
AlekseiKorolev <42728999+AlekseiKorolev@users.noreply.github.com>
2020-02-20 01:26:47 +0100

    Updates

---
+++ precache-manifest.de0ed677fb47074a79bf6c79e4f8056e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9176f3fff0f14fc648859251e967ee6",
+    "url": "/AXA-front-end-test/index.html"
+  },
+  {
+    "revision": "aa78a74bec444e721ded",
+    "url": "/AXA-front-end-test/static/css/main.b76e23f0.chunk.css"
+  },
+  {
+    "revision": "aec3f3118a4493186e5b",
+    "url": "/AXA-front-end-test/static/js/2.6a4c927c.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/AXA-front-end-test/static/js/2.6a4c927c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa78a74bec444e721ded",
+    "url": "/AXA-front-end-test/static/js/main.30c25839.chunk.js"
+  },
+  {
+    "revision": "6659e791a4d920431dd7",
+    "url": "/AXA-front-end-test/static/js/runtime-main.fc7af967.js"
+  },
+  {
+    "revision": "a6b9124c598ff3253a374ba6b34af3bb",
+    "url": "/AXA-front-end-test/static/media/city.a6b9124c.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/test/precache-manifest.2e02675a3ad423f602b7be12ac034585.js"
+  "/AXA-front-end-test/precache-manifest.de0ed677fb47074a79bf6c79e4f8056e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/test/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/AXA-front-end-test/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
